72108e0afa360d5e06ce0d68219a12cacaa1d14e
xch <158972928@qq.com>
2018-03-25 18:39:04 +0800

    asdfsdaf

--- routes/index.js
+++ routes/index.js
@@ -10,9 +10,20 @@ router.get('/getName', function(req, res, next) {
   	name: 'xch'
   });
 });
-router.get('/api/getName', function(req, res, next) {
+router.get('/api/getScore', function(req, res, next) {
+	let name = '其他人'
+	let score = 95
+	if (req.query.id==1) {
+		name = 'xxq'
+		score = 100
+	}
+	if (req.query.id==2){
+		name = 'xch'
+		score = 99
+	}
   res.json({
-  	name: 'xch'
+  	name,
+  	score,
   });
 });
 router.get('/api/getAge', function(req, res, next) {
